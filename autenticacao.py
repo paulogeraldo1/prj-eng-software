38630d90897438918c6d5a18ac1778c3646e55b5
paulogeraldo1 <pgls@ecomp.poli.br>
2024-03-11 00:49:24 -0300

    implemetando a conta conjunta

---
+++ autenticacao.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from functools import wraps
+
+usuarios = {'user': '1234', 'user2':'4321'}
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'usuario' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def autenticar():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    
+    if usuario in usuarios and usuarios[usuario] == senha:
+        session['usuario'] = usuario
+        return redirect('/menu')
+    else:
+        return render_template('login.html', error=True)
+    
+def logout():
+    global contas_a_pagar, contas_pagas, renda, saldo_restante, renda_2, saldo_restante_2
+    contas_a_pagar = []
+    contas_pagas = []
+    renda = 0
+    renda_2 = 0
+    saldo_restante = 0
+    saldo_restante_2 = 0
+    session.pop('usuario', None)
+    return redirect(url_for('login'))
